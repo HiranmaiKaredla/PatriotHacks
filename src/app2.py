a211e1c2ddb508ddc11aac9bf6ad6a6db9a817bd
Hiranmai Karedla <hiranmai@Hiranmais-MBP.eduroam.gmu.edu>
2024-10-12 16:55:09 -0400

    added buttons for query results

--- src/app2.py
+++ src/app2.py
@@ -10,6 +10,7 @@ import os
 from config import OPENAI_API_KEY
 
 os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
+st.header("PatriotHacks - Dream Home")
 
 df = pd.read_csv("./apartments_for_rent_classified_10K.csv", encoding="latin-1", sep=";")
 engine = create_engine("sqlite:///properties.db")
@@ -26,12 +27,29 @@ chain = create_sql_query_chain(llm, db)
 text_input = st.text_input("Enter your query")
 
 if text_input:
-    sql_query = chain.invoke({"question": "Get ids of {query}".format(query=text_input)})
+    sql_query = chain.invoke({"question": "Get ids of {query} limit 10".format(query=text_input)})
+    #print(sql_query)
     response = db.run(sql_query)
-    
+    #print(response)
     id_numbers = [int(num) for num in re.findall(r'\d+', response)]
-    filtered_df = df[df['id'].isin(id_numbers)]
-    st.dataframe(filtered_df)
+    if len(id_numbers):
+        filtered_df = df[df['id'].isin(id_numbers)]
+        # st.dataframe(filtered_df[['title', 'body', 'amenities', 'bathrooms', 'bedrooms', 'price', 'square_feet', 'cityname', 'state']]
+        #     )
+        for index, row in filtered_df.iterrows():
+            # Create a button for each property
+            if st.button(f"{row['title']} - ${row['price']}", key=row['id']):
+                # Display detailed information when the button is clicked
+                st.subheader(f"Details for {row['title']}")
+                st.write(f"Category: {row['category']}")
+                st.write(f"City: {row['cityname']}")
+                st.write(f"Bedrooms: {row['bedrooms']}, Bathrooms: {row['bathrooms']}")
+                st.write(f"Amenities: {row['amenities']}")
+                st.write(f"Price: ${row['price']}")
+                st.write(f"Property ID: {row['id']}")
+    else:
+        st.write("Sorry! we done have any results with the query")
+
 
 
 
